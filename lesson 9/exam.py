89a4bb8ec03de5ce75f2eb57767d9a20dc35959f
Manan595 <manan.patel498@education.nsw.gov.au>
2024-09-17 17:28:18 +1000

    exam

---
+++ lesson 9/exam.py
@@ -0,0 +1,10 @@
+medicalcause=input("do your have medical cause yes or no ")
+attendence=int(input("enter the attendence of the student"))
+if medicalcause=='yes':
+    print('you are allowed ')
+else:
+    if attendence>=75:
+     print('alowed')
+    else:
+       print('you are not allowed ')
+
---
+++ main1.py
@@ -0,0 +1,9 @@
+print("enter a NUmber ( Numeratar):")
+num=int(input())
+print("enter a number (denominatior):")
+numd=int(input())
+
+if num%numd==0:
+    print("\n" +str(num)+"is divisibel by" +str(numd))
+else:
+    print("\n" +str(num)+"is divisibel by" +str(numd))
